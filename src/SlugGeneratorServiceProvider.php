749b50b2f4fe7a49fad5d02ff878e1aea4486867
Piotr Lewandowski <piotr.lewandowski@kodooy.com>
2025-08-01 00:23:02 +0200

    Add SlugGeneratorServiceProvider to fix package installation

---
+++ src/SlugGeneratorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Kodooy\SlugGenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class SlugGeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
